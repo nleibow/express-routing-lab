1e6732a21a3bb448d3b15b2e64a0013e26a7af40
nleibow <nathan.leibowitz1@gmail.com>
2016-11-28 18:09:13 -0700

    all five funcs funking

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,17 +1,18 @@
 //Did you use npm install to
 //add all these packages
-//to our project?
+//to our project?---done
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-// How do we 'require' the candyRouter file?
-var candyRouter;
+// How do we 'require' the candyRouter file?---done
+var candyRouter = require('./candyRouter');
 
 app.use(bodyParser.json());
+app.use('/candy',candyRouter);
 
-//How do we redirect the /candies path
-//through our candyRouter?
-//Hint: you need app.use
+//How do we redirect the /candies path---done
+//through our candyRouter?---done
+//Hint: you need app.use---done
 
 app.listen(3000);
--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -2,15 +2,60 @@ var express = require('express');
 var bodyParser = require('body-parser');
 router = express.Router();
 
-var candies;
+var candies = [{"id":1,"name":"Chewing Gum","color":"Red"},{"id":2,"name":"Pez","color":"Green"},{"id":3,"name":"Marshmallow","color":"Pink"},{"id":4,"name":"Candy Stick","color":"Blue"}];
 
-//What would need to go into candies
+//What would need to go into candies--done
 //in order to pass our first test?
 
 router.get('/', function(req,res) {
-	// What would go here? 
-	// Hint: we want all candies in JSON format
+	res.json(candies);
+	// What would go here? --done
+	// Hint: we want all candies in JSON format--done
 });
+
+router.get('/:id', function(req, res){
+	var pos = req.params.id;
+	for (var i = 0; i <candies.length; i++) {
+	if (candies[i].id == pos){
+		res.json(candies[i]);
+	}
+	}
+	
+
+});
+
+router.post('/', function(req, res){
+	req.body.id = candies.length + 1;
+	candies.push(req.body);
+	res.json(req.body);
+});
+
+router.put('/:id', function(req, res){
+	var pos = req.params.id;
+		for (var i = 0; i <candies.length; i++) {
+			if (candies[i].id == pos){
+			candies[i].name =  req.body.name;
+			candies[i].color = req.body.color;
+			res.json(candies[i]);
+	}
+	
+}
+res.json(candies)
+;});
+
+router.delete('/:id', function(req, res){
+	var pos = req.params.id;
+	for (var i = 0; i <candies.length; i++) {
+	if (candies[i].id == pos){
+		res.json(candies.splice(i, 1));
+	}
+	
+	}
+	
+	res.json(candies);
+});
+
+
 
 // Fill out the rest of the routes here
 
